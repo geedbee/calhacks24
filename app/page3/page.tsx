538815fe6ace18d6a1cfe440beb7adc4a6f7bbc3
H3nky <102431386+H3nky@users.noreply.github.com>
2024-10-19 19:13:44 -0700

    Added webscraper demo.

--- app/page3/page.tsx
+++ app/page3/page.tsx
@@ -1,4 +1,36 @@
-// app/page3/page.tsx
+'use client'
+import ScraperView from "@/app/page3/webscraper";
+import { useEffect, useState } from 'react'
 export default function Page3() {
-  return <div>This is Page 3</div>;
+    let [scrapedPage, setScrapedPage] = useState("Loading...")
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const scrapePage = (input) => {
+        input.preventDefault(); // Prevent the default form submission
+        alert(inputValue)
+        ScraperView(inputValue)
+        .then((response) => setScrapedPage(response))
+        .catch((error) => setScrapedPage("Failed to load: "+error))
+    }
+   
+    return(
+    <div>
+       <form onSubmit={scrapePage}>
+            <label>
+                Text Input:
+                <input 
+                    type="text"
+                    value={inputValue} 
+                    onChange={handleChange} 
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+        {scrapedPage}
+    </div>
+    );
 }
