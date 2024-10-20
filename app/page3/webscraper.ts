d99d8959d49c4cd0729a73fa0382fa48ba999844
H3nky <102431386+H3nky@users.noreply.github.com>
2024-10-19 20:51:48 -0700

    Updated webscraper.ts

--- app/page3/webscraper.ts
+++ app/page3/webscraper.ts
@@ -1,25 +1,18 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-import Axios, {AxiosResponse} from 'axios'
+import Axios from 'axios'
 import * as cheerio from 'cheerio'
 
 const elements: string = 'p, h1, h2'
 export default async function getHTML(url: string) {
     const response = await Axios.get(url)
-    const parsed = parseResponse(response)
-    //TODO remove log statement
-    return parsed
-}
-
-//Parses a web page retrieved with Axios using cheerio. 
-function parseResponse(response: AxiosResponse) {
     const $ = cheerio.load(response.data);
     const paragraphElements = $(elements)
     const textElementArray = paragraphElements.map((index, element) => $(element).text()).get();
-    let mergedString: string = ""
+    let parsedString: string = ""
     textElementArray.forEach((text) => {
-        mergedString += (text.charAt(text.length-1) === '.')? ". " : ' ' + text
+        parsedString += (text.charAt(text.length-1) === '.')? ". " : ' ' + text
     })
-    return mergedString
+    return parsedString
 }
 
 
